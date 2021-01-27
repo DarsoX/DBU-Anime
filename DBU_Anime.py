76ac239641b15d3af5ce8daa542d847d57cf3c3c
DarsoX <43499767+DarsoX@users.noreply.github.com>
2021-01-27 03:14:05 +0200

    Add files via upload

---
+++ DBU_Anime.py
@@ -0,0 +1,30 @@
+import http.client
+
+conn = http.client.HTTPConnection("127.0.0.1:5000")
+
+def get_topanime(years = 2020):
+    conn.request("GET", "/aimedbu?method=get_topanime&year={}".format(years))
+
+    res = conn.getresponse()
+    data = res.read()
+
+    return data.decode("utf-8")
+
+def get_anime():
+    conn.request("GET", "/aimedbu?method=get_anime")
+
+    res = conn.getresponse()
+    data = res.read()
+
+    return data.decode("utf-8")
+
+def get_animejanr(janr = ''):
+    if janr != '':
+        onn.request("GET", "/aimedbu?method=get_animejanr&janr={}".format(janr))
+
+        res = conn.getresponse()
+        data = res.read()
+
+        return data.decode("utf-8")
+    else:
+        return 'DBU_EROR: Укажите Жанр.'
